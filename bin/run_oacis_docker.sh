f1fd180e3b6e4dbc2ed7655e4efaddbf3c72320a
Takeshi Uchitane <t.uchitane@gmail.com>
2015-01-16 11:27:17 +0900

    MongoDB is started by oacis user

--- bin/run_oacis_docker.sh
+++ bin/run_oacis_docker.sh
@@ -22,13 +22,12 @@ then
     mkdir ${WORKDIR}/.ssh
     chmod 700 ${WORKDIR}/.ssh
   fi
-  #docker run --name OACIS-${PROJECT_NAME}-DATA -v ${WORKDIR}/db:/data/db -v ${WORKDIR}/Result_development:/home/oacis/oacis/Result_development -v ${WORKDIR}/.ssh:/home/oacis/.ssh busybox
   echo "create new data container for ${PROJECT_NAME}"
 else
   echo "A container named ${PROJECT_NAME} exists."
   exit -1
 fi
 
-docker run -it -p $PORT:3000 --name OACIS-${PROJECT_NAME} -v ${WORKDIR}/db:/data/db -v ${WORKDIR}/Result_development:/home/oacis/oacis/public/Result_development -v ${WORKDIR}/work:/home/oacis/work -v ${WORKDIR}/.ssh:/home/oacis/.ssh ${OACIS_IMAGE} "su - oacis; /bin/bash"
+docker run -it -p $PORT:3000 --name OACIS-${PROJECT_NAME} -v ${WORKDIR}/db:/home/oaics/db -v ${WORKDIR}/Result_development:/home/oacis/oacis/public/Result_development -v ${WORKDIR}/work:/home/oacis/work -v ${WORKDIR}/.ssh:/home/oacis/.ssh ${OACIS_IMAGE} "su - oacis; /bin/bash"
 docker rm OACIS-${PROJECT_NAME}
 exit 0
