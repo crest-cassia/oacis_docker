4cf79a0d9bcbb7003050c6a73a8e98ad33f9251c
takeshi-uchitane <t.uchitane@gmail.com>
2015-10-28 17:49:04 +0900

    catch return codes from test scripts

--- test/test_all.sh
+++ test/test_all.sh
@@ -4,11 +4,21 @@ for script in `ls ./test/run/*.sh`
 do
   echo "$script"
   $script
+  rc=$?
+  if [ $rc -ne 0 ]
+  then
+    exit $rc
+  fi
 done
 
 for script in `ls ./test/dump_restore/*.sh`
 do
   echo "$script"
   $script
+  rc=$?
+  if [ $rc -ne 0 ]
+  then
+    exit $rc
+  fi
 done
 
