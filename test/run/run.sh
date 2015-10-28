93757a04cb33d4d33081023d9472b7a941768891
takeshi-uchitane <t.uchitane@gmail.com>
2015-10-28 17:29:39 +0900

    refactoring scripts and error scripts is temporary added

--- test/run/run.sh
+++ test/run/run.sh
@@ -3,8 +3,7 @@
 . ./test/base.sh
 
 function run_oacis() {
-  docker run --name oacis-mongo -d mongo:3.0.3
-  docker run --name oacis -i -p ${PORT}:3000 --link oacis-mongo:mongo -d ${OACIS_IMAGE}
+  docker run --name oacis -i -p ${PORT}:3000 -d ${OACIS_IMAGE}
   sleep 20
   docker logs oacis
   curl localhost:${PORT}
--- test/run/run_with_data_container.sh
+++ test/run/run_with_data_container.sh
@@ -4,9 +4,8 @@
 
 function run_with_data_container() {
   echo "hello" > aaa.txt
-  docker run --name oacis-mongo -d mongo:3.0.3
   docker create --name oacis-data -v `pwd`:/home/oacis/oacis/public/Result_development busybox
-  docker run --name oacis -i -p ${PORT}:3000 --link oacis-mongo:mongo -d --volumes-from oacis-data ${OACIS_IMAGE}
+  docker run --name oacis -i -p ${PORT}:3000 -d --volumes-from oacis-data ${OACIS_IMAGE}
   sleep 5
   test $(echo `docker exec -it oacis bash -c "cat /home/oacis/oacis/public/Result_development/aaa.txt"` | tr -d '\r') == "hello"
 }
