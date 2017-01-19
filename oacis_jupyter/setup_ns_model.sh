b8d11cc796ad3f0460d3bd005121c77d738174a7
Yohsuke Murase <yohsuke.murase@gmail.com>
2017-01-19 15:47:27 +0900

    added a script to install NS model

---
+++ oacis_jupyter/setup_ns_model.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -eux
+git clone https://github.com/yohm/nagel_schreckenberg_model.git
+cd nagel_schreckenberg_model
+bundle install --path=vendor/bundle
+$OACIS_ROOT/bin/oacis_ruby register_to_oacis.rb
+
