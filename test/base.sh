00c5219e8859f51a43f38f0c50ccc0cceaa48710
Yohsuke Murase <yohsuke.murase@gmail.com>
2017-01-24 11:16:22 +0900

    fixed the number of trials

--- test/base.sh
+++ test/base.sh
@@ -4,7 +4,7 @@ set -ux
 
 function run_oacis() {
   docker run --name ${OACIS_CONTAINER_NAME} -p 127.0.0.1:${PORT}:3000 -d ${OACIS_IMAGE} || return 1
-  for i in {0..1}
+  for i in {0..20}
   do
     sleep 3
     curl localhost:${PORT} && return 0
