ab75dd56ed641f4774e17881f60eec79c04469b3
Yohsuke Murase <yohsuke.murase@gmail.com>
2020-01-05 10:31:57 +0900

    version 3.7.1

--- version_tagging.sh
+++ version_tagging.sh
@@ -2,15 +2,18 @@
 
 # before running this script, run `docker login`
 set -eux
-OACIS_VERSION="v3.7.0"
+OACIS_VERSION="v3.7.1"
 
 docker login
 SCRIPT_DIR=$(cd $(dirname $0);pwd)
 cd $SCRIPT_DIR/oacis
 docker build . -t oacis/oacis:${OACIS_VERSION} --build-arg OACIS_VERSION=${OACIS_VERSION}
 docker push oacis/oacis:${OACIS_VERSION}
+docker tag oacis/oacis:${OACIS_VERSION} oacis/oacis:latest
+docker push oacis/oacis:latest
 
 cd $SCRIPT_DIR/oacis_jupyter
 docker build . -t oacis/oacis_jupyter:${OACIS_VERSION} --build-arg OACIS_VERSION=${OACIS_VERSION}
 docker push oacis/oacis_jupyter:${OACIS_VERSION}
-
+docker tag oacis/oacis_jupyter:${OACIS_VERSION} oacis/oacis_jupyter:latest
+docker push oacis/oacis_jupyter:latest
