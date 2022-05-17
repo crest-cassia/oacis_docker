4758e540613de710b37d0a2a95c04dcfa04e4089
Yohsuke Murase <yohsuke.murase@gmail.com>
2022-05-17 14:50:16 +0900

    add docker-host by default

--- oacis/oacis_start.sh
+++ oacis/oacis_start.sh
@@ -42,6 +42,11 @@ if [ "$(mongo ${db_name} --eval 'printjson(db.hosts.count({"name": "localhost"})
 then
   mongo ${db_name} --eval 'db.hosts.insert({"status" : "enabled", "work_base_dir" : "~/oacis/public/Result_development/work/__work__", "mounted_work_base_dir" : "~/oacis/public/Result_development/work/__work__", "max_num_jobs" : 4, "polling_interval" : 5, "min_mpi_procs" : 1, "max_mpi_procs" : 1, "min_omp_threads" : 1, "max_omp_threads" : 1, "name" : "localhost"})'
 fi
+if [ "$(mongo ${db_name} --eval 'printjson(db.hosts.count({"name": "docker-host"}));' | tail -1 | tr -d '\r')" == "0" ]
+then
+  mongo ${db_name} --eval 'db.hosts.insert({"status" : "enabled", "work_base_dir" : "~/oacis_work", "mounted_work_base_dir" : "", "max_num_jobs" : 1, "polling_interval" : 5, "min_mpi_procs" : 1, "max_mpi_procs" : 1, "min_omp_threads" : 1, "max_omp_threads" : 1, "name" : "docker-host"})'
+fi
+
 
 #run oacis
 # pre-compile assets to accelerate the boot
