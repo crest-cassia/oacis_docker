245acc4146b7a0caa1ebfa2ede7ca679bbfa5523
Yohsuke Murase <yohsuke.murase@gmail.com>
2025-07-30 14:37:16 +0900

    initial commit of splitting containers

--- oacis/oacis_start.sh
+++ oacis/oacis_start.sh
@@ -24,13 +24,6 @@ fi
 #start mongod, redis and sshd
 /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
 
-#waiting for mongod boot
-until [ "$(mongosh --eval 'printjson(db.serverStatus().ok)' | tail -1 | tr -d '\r')" == "1" ]
-do
-  sleep 1
-  echo "waiting for mongod boot..."
-done
-
 function cleanup() {
   set -x
   su - -c "echo terminating; cd ~/oacis; bundle exec rake daemon:stop" oacis
@@ -39,17 +32,6 @@ function cleanup() {
 }
 
 trap cleanup SIGINT SIGTERM
-
-db_name=oacis_development
-if [ "$(mongosh ${db_name} --eval 'printjson(db.hosts.count({"name": "localhost"}));' | tail -1 | tr -d '\r')" == "0" ]
-then
-  mongosh ${db_name} --eval 'db.hosts.insert({"status" : "enabled", "work_base_dir" : "~/oacis/public/Result_development/work/__work__", "mounted_work_base_dir" : "~/oacis/public/Result_development/work/__work__", "max_num_jobs" : 4, "polling_interval" : 5, "min_mpi_procs" : 1, "max_mpi_procs" : 1, "min_omp_threads" : 1, "max_omp_threads" : 1, "name" : "localhost"})'
-fi
-if [ "$(mongosh ${db_name} --eval 'printjson(db.hosts.count({"name": "docker-host"}));' | tail -1 | tr -d '\r')" == "0" ]
-then
-  mongosh ${db_name} --eval 'db.hosts.insert({"status" : "enabled", "work_base_dir" : "~/oacis_work", "mounted_work_base_dir" : "", "max_num_jobs" : 1, "polling_interval" : 5, "min_mpi_procs" : 1, "max_mpi_procs" : 1, "min_omp_threads" : 1, "max_omp_threads" : 1, "name" : "docker-host"})'
-fi
-
 
 #run oacis
 # pre-compile assets to accelerate the boot
